35b4ff0fe33a6e8710532d1c7671ea827ca950f6
Catur Purbo Yuwono <catur.purbo@fit.id>
2024-04-28 09:45:00 +0700

    added config for gcp and logback

--- src/main/java/com/comfozen/contractmanagement/service/ContractService.java
+++ src/main/java/com/comfozen/contractmanagement/service/ContractService.java
@@ -9,16 +9,15 @@ import com.comfozen.contractmanagement.dto.Contract;
 
 @Service
 public class ContractService {
-
   private List<Contract> contracts = new ArrayList<>();
 
   public List<Contract> getAllContracts() {
-    System.out.println("count current contracts " + contracts.size());
+
     return contracts;
   }
 
   public void addContract(Contract contract) {
-    System.out.println("starting to add contract " + contract.getName());
+
     contracts.add(contract);
   }
 }
